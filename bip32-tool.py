6cda79ed401478fc12abab1152c74294d165266f
Belcher <belcher@email>
2014-12-25 17:50:50 +0000

    shows you address balances now
    [gitreformat yapf-ify (github/ghtdak) on Fri Dec  4 04:41:09 2015]
    [from commit: 6ee6886f668e80f0a1ecd4aa354de9b5619d3f2c]

--- bip32-tool.py
+++ bip32-tool.py
@@ -1,5 +1,8 @@
 import bitcoin as btc
+from common import Wallet
+
 import sys
+from optparse import OptionParser
 
 #structure for cj market wallet
 # m/0/ root key
@@ -10,86 +13,55 @@ import sys
 # m/0/n/0/k kth receive address, for mixing depth n
 # m/0/n/1/k kth change address, for mixing depth n
 
-seed = sys.argv[1]  #btc.sha256('dont use brainwallets')
+parser = OptionParser(
+    usage='usage: %prog [options] [seed]',
+    description='Does useful little lasts involving your bip32 wallet.')
+parser.add_option('-p',
+                  '--privkey',
+                  action='store_true',
+                  dest='showprivkey',
+                  help='print private key along with address')
+parser.add_option('-m',
+                  '--maxmixdepth',
+                  action='store',
+                  type='int',
+                  dest='maxmixdepth',
+                  default=2,
+                  help='maximum mixing depth to look for')
+parser.add_option('-g',
+                  '--gap-limit',
+                  action='store',
+                  dest='gaplimit',
+                  help='gap limit for wallet',
+                  default=6)
+(options, args) = parser.parse_args()
+
+if len(args) != 1:
+    parser.error('Needs a seed')
+    sys.exit(0)
+seed = args[0]
 #seed = '256 bits of randomness'
 
-print_privkey = len(sys.argv) > 2
+print_privkey = options.showprivkey
 
-master = btc.bip32_master_key(seed)  #, btc.TESTNET_PRIVATE)
-print 'master = ' + master
+wallet = Wallet(seed, options.maxmixdepth)
+print 'downloading wallet history'
+wallet.download_wallet_history(options.gaplimit)
+wallet.find_unspent_addresses()
 
-addr_vbyte = 0x6f  #testnet
-
-m_0 = btc.bip32_ckd(master, 0)
-for n in range(2):
-    print 'mixing depth ' + str(n) + ' m/0/' + str(n) + '/'
-    m_0_n = btc.bip32_ckd(m_0, n)
-    for forchange in range(2):
+for m in range(wallet.max_mix_depth):
+    print 'mixing depth %d m/0/%d/' % (m, m)
+    for forchange in [0, 1]:
         print(' ' +
               ('receive'
                if forchange == 0 else 'change') + ' addresses m/0/%d/%d/' %
-              (n, forchange))
-        m_0_n_c = btc.bip32_ckd(m_0_n, forchange)
-        for k in range(3):
-            m_0_n_c_k = btc.bip32_ckd(m_0_n_c, k)
-            priv = btc.bip32_extract_key(m_0_n_c_k)
-            output = btc.privtoaddr(priv, addr_vbyte)
-            if print_privkey:
-                output += ' ' + btc.encode_privkey(priv, 'wif', addr_vbyte)
-            print '  m/0/%d/%d/%d/ ' % (n, forchange, k) + output
-'''
-#default key on http://bip32.org/
-m_priv =\
-'xprv9s21ZrQH143K2JF8RafpqtKiTbsbaxEeUaMnNHsm5o6wCW3z8ySyH4UxFVSfZ8n7ESu7fgir8imbZKLYVBxFPND1pniTZ81vKfd45EHKX73'
-
-m_pub = btc.bip32_privtopub(m_priv)
-print 'm_pub = ' + m_pub
-
-print 'prv(hex) = ' + btc.bip32_extract_key(m_priv)
-print 'prv(wif) = ' + btc.encode_privkey(btc.bip32_extract_key(m_priv), 'wif_compressed')
-print 'pub(hex) = ' + btc.bip32_extract_key(m_pub)
-print 'addr = ' + btc.pubtoaddr(btc.bip32_extract_key(m_pub))
-
-def print_pub_priv(prefix, priv):
-	pub = btc.bip32_privtopub(priv)
-	print prefix
-	print 'pub = ' + pub
-	print 'prv = ' + priv
-
-#bip32 test vector
-print '\nbip32 test vector\n'
-m_priv =\
-'xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi'
-
-#i_H = i + 2**31
-chain_m_0h = btc.bip32_ckd(m_priv, 0 + 2**31)
-print_pub_priv('chain m/0H', chain_m_0h)
-
-chain_m_0h_1 = btc.bip32_ckd(chain_m_0h,  1)
-print_pub_priv('chain m/0H/1', chain_m_0h_1)
-
-chain_m_0h_1_2h = btc.bip32_ckd(chain_m_0h_1,  2 + 2**31)
-print_pub_priv('chain m/0H/1/2H', chain_m_0h_1_2h)
-
-
-#bip32 test vector 2
-print '\nbip32 test vector 2\n'
-m_priv =\
-'xprv9s21ZrQH143K31xYSDQpPDxsXRTUcvj2iNHm5NUtrGiGG5e2DtALGdso3pGz6ssrdK4PFmM8NSpSBHNqPqm55Qn3LqFtT2emdEXVYsCzC2U'
-
-print 'master(hex) = ' + btc.bip32_extract_key(m_priv)
-
-chain_m_0 = btc.bip32_ckd(m_priv, 0)
-print_pub_priv('chain m/0', chain_m_0)
-
-chain_m_0_214blahH = btc.bip32_ckd(chain_m_0,  2147483647 + 2**31)
-print_pub_priv('chain m/0/2147483647H', chain_m_0_214blahH)
-
-chain_m_0_214blahH_1 = btc.bip32_ckd(chain_m_0_214blahH, 1)
-print_pub_priv('chain m/0/2147483647H/1', chain_m_0_214blahH_1)
-'''
-'''
-seed = '256 bits of randomness'
-m_priv = btc.bip32_master_key(seed)
-print 'new seed = ' + m_priv
-'''
+              (m, forchange))
+        for k in range(wallet.index[m][forchange] + options.gaplimit):
+            addr = wallet.get_addr(m, forchange, k)
+            balance = 0.0
+            for addrvalue in wallet.unspent.values():
+                if addr == addrvalue['address']:
+                    balance += addrvalue['value']
+            used = ('used' if k < wallet.index[m][forchange] else ' new')
+            print '  m/0/%d/%d/%2d %s %s %.8fbtc' % (m, forchange, k, addr,
+                                                     used, balance / 1e8)
