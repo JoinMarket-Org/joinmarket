7a21a074e5bdf9f66f24f830ac530fe1f008dca7
Belcher <belcher@email>
2015-01-23 22:20:07 +0000

    added some more debug prints to help fix a bug

--- common.py
+++ common.py
@@ -107,6 +107,7 @@ class Wallet(object):
 				continue
 			removed_utxos[utxo] = self.unspent[utxo]
 			del self.unspent[utxo]
+		debug('removed utxos, wallet now is \n' + pprint.pformat(self.get_mix_utxo_list()))
 		return removed_utxos
 
 	def add_new_utxos(self, tx, txid):
@@ -119,6 +120,7 @@ class Wallet(object):
 			utxo = txid + ':' + str(index)
 			added_utxos[utxo] = addrdict
 			self.unspent[utxo] = addrdict
+		debug('added utxos, wallet now is \n' + pprint.pformat(self.get_mix_utxo_list()))
 		return added_utxos
 
 	def get_mix_utxo_list(self):
