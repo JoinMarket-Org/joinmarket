d400b53a406822d7bcddb37084c1afb7524b5c8f
Belcher <belcher@email>
2015-02-16 00:02:46 +0000

    first start at tumbler.py

--- tumbler.py
+++ tumbler.py
@@ -1,107 +1,110 @@
 
+from optparse import OptionParser
+import datetime
+import numpy as np
+from pprint import pprint
 
-algo_thread = None
+def int_lower_bounded(thelist, lowerbound):
+	return [int(l) if int(l) >= lowerbound else lowerbound for l in thelist]
 
-#how long to wait for all the orders to arrive before starting to do coinjoins
-ORDER_ARRIVAL_WAIT_TIME = 2
+def generate_tumbler_tx(destaddrs, options):
+	#sends the coins up through a few mixing depths
+	#send to the destination addresses from different mixing depths
 
-def choose_order(cj_amount):
-	
-	sqlorders = db.execute('SELECT * FROM orderbook;').fetchall()
-	orders = [(o['counterparty'], o['oid'],	calc_cj_fee(o['ordertype'], o['cjfee'], cj_amount))
-		for o in sqlorders if cj_amount >= o['minsize'] or cj_amount <= o['maxsize']]
-	orders = sorted(orders, key=lambda k: k[2])
-	print 'orders = ' + str(orders)
-	return orders[0] #choose the cheapest, later this will be chosen differently
+	#simple algo, move coins completely from one mixing depth to the next
+	# until you get to the end, then send to destaddrs
 
-def choose_sweep_order(my_total_input, my_tx_fee):
-	'''
-	choose an order given that we want to be left with no change
-	i.e. sweep an entire group of utxos
+	#txcounts for going completely from one mixdepth to the next
+	# follows a normal distribution
+	txcounts = np.random.normal(options.txcountparams[0],
+		options.txcountparams[1], options.mixdepthcount)
+	txcounts = int_lower_bounded(txcounts, 1)
 
-	solve for mychange = 0
-	ABS FEE
-	mychange = totalin - cjamount - mytxfee - absfee
-	=> cjamount = totalin - mytxfee - absfee
-	REL FEE
-	mychange = totalin - cjamount - mytxfee - relfee*cjamount
-	=> 0 = totalin - mytxfee - cjamount*(1 + relfee)
-	=> cjamount = (totalin - mytxfee) / (1 + relfee)
-	'''
-	def calc_zero_change_cj_amount(ordertype, cjfee):
-		cj_amount = None
-		if ordertype == 'absorder':
-			cj_amount = my_total_input - my_tx_fee - cjfee
-		elif ordertype == 'relorder':
-			cj_amount = (my_total_input - my_tx_fee) / (Decimal(cjfee) + 1)
-			cj_amount = int(cj_amount.quantize(Decimal(1)))
-		else:
-			raise RuntimeError('unknown order type: ' + str(ordertype))
-		return cj_amount
-
-	sqlorders = db.execute('SELECT * FROM orderbook;').fetchall()
-	orders = [(o['counterparty'], o['oid'],	calc_zero_change_cj_amount(o['ordertype'], o['cjfee']),
-		o['minsize'], o['maxsize']) for o in sqlorders]
-	#filter cj_amounts that are not in range
-	orders = [o[:3] for o in orders if o[2] >= o[3] and o[2] <= o[4]]
-	orders = sorted(orders, key=lambda k: k[2])
-	print 'sweep orders = ' + str(orders)
-	return orders[-1] #choose one with the highest cj_amount, most left over after paying everything else
-
-#thread which does the buy-side algorithm
-# chooses which coinjoins to initiate and when
-class AlgoThread(threading.Thread):
-	def __init__(self, taker, initial_unspents):
-		threading.Thread.__init__(self)
-		self.daemon = True
-		self.taker = taker
-		self.initial_unspents = initial_unspents
-		self.finished_cj = False
-
-	def finished_cj_callback(self):
-		self.finished_cj = True
-		print 'finished cj'
-
-	def run(self):
-		global cjtx
-		time.sleep(ORDER_ARRIVAL_WAIT_TIME)
-		#while True:
-		if 1:
-			#wait for orders to arrive
-			#TODO just make this do one tx and then stop
-			if len(self.initial_unspents) == 0:
-				print 'finished mixing, closing...'
-				self.taker.shutdown()
-				#break
-
-			#utxo, addrvalue = self.initial_unspents.popitem()
-			utxo, addrvalue = [(k, v) for k, v in self.initial_unspents.iteritems() if v['value'] == 200000000][0]
-			counterparty, oid, cj_amount = choose_sweep_order(addrvalue['value'], my_tx_fee)
-			self.finished_cj = False
-			cjtx = CoinJoinTX(self.taker, cj_amount, [counterparty], [int(oid)],
-				[utxo], self.taker.wallet.get_receive_addr(mixing_depth=1), None,
-				my_tx_fee, self.finished_cj_callback)
-			#algorithm for making 
-			'''
-			single_cj_amount = 112000000
-			unspent = []
-			for utxo, addrvalue in self.initial_unspents.iteritems():
-				unspent.append({'value': addrvalue['value'], 'utxo': utxo})
-			inputs = btc.select(unspent, single_cj_amount)
-			my_utxos = [i['utxo'] for i in inputs]
-			counterparty, oid = choose_order(single_cj_amount)
-			cjtx = CoinJoinTX(self.irc, int(single_cj_amount), [counterparty], [int(oid)],
-				my_utxos, wallet.get_receive_addr(mixing_depth=1), wallet.get_change_addr(mixing_depth=0))
-			'''
-			while not self.finished_cj:
-				time.sleep(5)
-			print 'woken algo thread'
+	tx_list = []
+	for m, txcount in enumerate(txcounts):
+		#assume that the sizes of outputs will follow a power law
+		amount_ratios = np.random.power(options.amountpower, txcount)
+		amount_ratios /= sum(amount_ratios)
+		#transaction times are uncorrelated and therefore follow poisson
+		blockheight_waits = np.random.poisson(options.timelambda, txcount)
+		#number of makers to use follows a normal distribution
+		makercounts = np.random.normal(options.makercountrange[0], options.makercountrange[1], txcount)
+		makercounts = int_lower_bounded(makercounts, 2)
+		for amount_ratio, blockheight_wait, makercount in zip(amount_ratios, blockheight_waits, makercounts):
+			tx = {'amount_ratio': amount_ratio, 'blockheight_wait': blockheight_wait,
+				'srcmixdepth': m + options.mixdepthsrc, 'makercount': makercount}
+			tx_list.append(tx)
+	pprint(tx_list)
+	block_count = sum([tx['blockheight_wait'] for tx in tx_list])
+	print 'requires ' + str(block_count) + ' blocks'
+	print('estimated time taken ' + str(block_count*10) +
+		' minutes or ' + str(block_count/6.0) + ' hours')
+	maker_count = sum([tx['makercount'] for tx in tx_list])
+	relorder_fee = 0.001
+	print('uses ' + str(maker_count) + ' makers, at ' + str(relorder_fee*100) + '% per maker, estimated total cost '
+		+ str(round((1 - (1 - relorder_fee)**maker_count) * 100, 3)) + '%')
 
 def main():
-	print 'downloading wallet history'
-	wallet = Wallet(seed)
-	wallet.download_wallet_history()
-	wallet.find_unspent_addresses()
+	parser = OptionParser(usage='usage: %prog [options] [seed] [tumble-file / destaddr...]',
+		description='Sends bitcoins to many different addresses using coinjoin in'
+			' an attempt to break the link between them. Sending to multiple '
+			' addresses is highly recommended for privacy. This tumbler can'
+			' be configured to ask for more address mid-run, giving the user'
+			' a chance to click `Generate New Deposit Address` on whatever service'
+			' they are using.')
+	parser.add_option('-m', '--mixdepthsource', type='int', dest='mixdepthsrc',
+		help='mixing depth to spend from, default=0', default=0)
+	parser.add_option('-f', '--txfee', type='int', dest='txfee',
+		default=10000, help='miner fee contribution, in satoshis, default=10000')
+	parser.add_option('-a', '--addrask', type='int', dest='addrask',
+		default=2, help='How many more addresses to ask for in the terminal, default=2')
+	parser.add_option('-N', '--makercountrange', type='float', nargs=2, action='store',
+		dest='makercountrange',
+		help='Input the range of makers to use. e.g. 3-5 will random use between '
+		'3 and 5 makers inclusive, default=3 4', default=(3, 1))
+	parser.add_option('-M', '--mixdepthcount', type='int', dest='mixdepthcount',
+		help='how many mixing depths to mix through', default=3)
+	parser.add_option('-c', '--txcountparams', type='float', nargs=2, dest='txcountparams', default=(5, 1),
+		help='The number of transactions to take coins from one mixing depth to the next, it is'
+		' randomly chosen following a normal distribution. This option controlled the parameters'
+		' of that normal curve. (mean, standard deviation). default=(5, 1)')
+	parser.add_option('--amountpower', type='float', dest='amountpower', default=3.0,
+		help='the output amounts follow a power law distribution, this is the power, default=3.0')
+	parser.add_option('-l', '--timelambda', type='float', dest='timelambda', default=2,
+		help='the number of blocks to wait between transactions is randomly chosen '
+		' following a poisson distribution. This parameter is the lambda of that '
+		' distribution. default=2 blocks')
+
+	parser.add_option('-w', '--wait-time', action='store', type='float', dest='waittime',
+		help='wait time in seconds to allow orders to arrive, default=5', default=5)
+	(options, args) = parser.parse_args()
+
+	if len(args) < 2:
+		parser.error('Needs a seed and destination addresses')
+		sys.exit(0)
+	seed = args[0]
+	destaddrs = args[1:]
+
+	if len(destaddrs) + options.addrask <= 1:
+		print '='*50
+		print 'WARNING: You are only using one destination address'
+		print 'this is almost useless for privacy'
+		print '='*50
+
+	print 'seed=' + seed
+	print 'destaddrs=' + str(destaddrs)
+	print str(options)
+	generate_tumbler_tx(destaddrs, options)
+
+	#a couple of overarching modes
+	#im-running-from-the-nsa, takes about 80 hours, costs a lot
+	#python tumbler.py -N 10 5 -c 10 5 -l 5 -M 10 seed 1
+	#
+	#quick and cheap, takes about 90 minutes
+	#python tumbler.py -N 2 1 -c 3 0.001 -l 2 -M 2 seed 1
+	#
+	#default, good enough for most, takes about 5 hours
+	#python tumbler.py seed 1
 
 if __name__ == "__main__":
 	main()
