f290cd69bdd136f4483494d6d6ecdb75c46f7d43
Adam Gibson <ekaggata@gmail.com>
2015-02-21 19:34:12 +0200

    reset blockchain source to blockr by default
    [gitreformat yapf-ify (github/ghtdak) on Fri Dec  4 04:43:25 2015]
    [from commit: ea829adcceaaf165e186aa3734e54b600c8b32af]

--- common.py
+++ common.py
@@ -13,7 +13,7 @@ PORT = 6667
 
 COMMAND_PREFIX = '!'
 MAX_PRIVMSG_LEN = 400
-blockchain_source = 'regtest'
+blockchain_source = 'blockr'
 btc_cli_loc = ''
 ordername_list = ["absorder", "relorder"]
 encrypted_commands = ["auth", "ioauth", "tx", "sig"]
@@ -116,6 +116,8 @@ def get_blockchain_data(body,
 
 
 def get_blockr_data(req):
+    if 'tx/push' in req:  #manually parse this; special case because POST not GET?
+        return btc.blockr_pushtx(req.split('/')[-1], get_network())
     return btc.make_request(req)
 
 
