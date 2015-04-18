9f4b5b4ad16c035dc439169e4881bf70a22a7c4c
Belcher <belcher@email>
2015-04-18 01:30:59 +0100

    fixed stupid bug
    [gitreformat yapf-ify (github/ghtdak) on Fri Dec  4 04:45:51 2015]
    [from commit: 671b34e1448ef39a8e39b02c64adabe0ee79747e]

--- lib/blockchaininterface.py
+++ lib/blockchaininterface.py
@@ -299,7 +299,8 @@ class NotifyRequestHeader(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
         if self.path.startswith('/walletnotify?'):
             txid = self.path[len(pages[0]):]
-            txd = btc.deserialize(self.rpc(['getrawtransaction', txid]).strip())
+            txd = btc.deserialize(self.btcinterface.rpc(['getrawtransaction',
+                                                         txid]).strip())
             tx_output_set = set([(sv['script'], sv['value']) for sv in txd[
                 'outs']])
 
