9b993097d890d86c5d6724b13a7d4f4fcb5847dc
chris-belcher <chris-belcher@users.noreply.github.com>
2015-05-23 21:24:12 +0100

    handled the case where len(orders) == n

--- lib/common.py
+++ lib/common.py
@@ -314,7 +314,10 @@ def weighted_order_choose(orders, n, feekey):
 	else:
 		phi = feekey(orders[-1]) - minfee
 	fee = np.array([feekey(o) for o in orders])
-	weight = np.exp(-(1.0*fee - minfee) / phi)
+	if phi > 0:
+		weight = np.exp(-(1.0*fee - minfee) / phi)
+	else:
+		weight = np.ones_like(fee)
 	weight /= sum(weight)
 	debug('randomly choosing orders with weighting\n' + pprint.pformat(zip(orders, weight)))
 	chosen_order_index = np.random.choice(len(orders), p=weight)
