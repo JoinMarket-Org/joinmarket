f663d3255a85d2a609371180edd690bdcaddd457
chris-belcher <chris-belcher@users.noreply.github.com>
2015-05-24 23:48:53 +0100

    removed duplicate check for utxos being spent, required to remove references to wallet.unspent
    [gitreformat yapf-ify (github/ghtdak) on Fri Dec  4 04:47:40 2015]
    [from commit: 52dd847db537b76a315625b27f7e11c707affad4]

--- lib/maker.py
+++ lib/maker.py
@@ -143,11 +143,8 @@ class CoinJoinOrder(object):
          not in input_addresses:
             return False, "authenticating bitcoin address is not contained"
         my_utxo_set = set(self.utxos.keys())
-        wallet_utxos = set(self.maker.wallet.unspent)
         if not tx_utxo_set.issuperset(my_utxo_set):
             return False, 'my utxos are not contained'
-        if not wallet_utxos.issuperset(my_utxo_set):
-            return False, 'my utxos already spent'
 
         my_total_in = sum([va['value'] for va in self.utxos.values()])
         self.real_cjfee = calc_cj_fee(self.ordertype, self.cjfee,
