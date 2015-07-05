57eaf4e8ae7a25d4df0a1af5e2748d3cd530f455
chris-belcher <chris-belcher@users.noreply.github.com>
2015-07-05 18:27:47 +0100

    meta-commit: fixes to new yield generator algorithm
    This is a combination of 11 commits:
    - fixed bug in new yield gen algo with too-low balances, wrote some explaination
    - commented out one line, both work but theres a tradeoff between ease of use and ease of understanding
    - fixed bug where cjfee was announced backwards
    - fixed bug with choosing mixdepth for absorder
    - fixed bug involving wrong value for minsize, added more debug lines
    - fixed error in certain situations, absorder isnt always announced
    - fixed crash when it assumed an absorder was announced
    - attempted to fix order management problem that was causing crashes
    - fixed silly bug
    - recoded handling of absorders that caused a crash
    - no longer announces the absorder unnecessarily

--- yield-generator.py
+++ yield-generator.py
@@ -11,20 +11,20 @@ import bitcoin as btc
 import common, blockchaininterface
 
 from socket import gethostname
-
-#txfee = 1000
-# tx fees for available mix levels from max to min amounts. mix_levels is max. 
-#txfee = [1000,1000,100,100]
-txfee = 1000
-# fees for available mix levels from max to min amounts. mix_levels is max
-cjfee = ['0.0009','0.0008', '0.0007','0.0006', '0.0005']
-nickname = random_nick()
-nickserv_password = ''
-# minsize is dynamic and defined during orders creation in create_my_orders
-#minsize = int(1.2 * txfee / float(cjfee))
 mix_levels = 5
 
+#CONFIGURATION
 
+#miner fee contribution
+txfee = 5000
+# fees for available mix levels from max to min amounts.
+cjfee = ['0.00015', '0.00014', '0.00013', '0.00012', '0.00011']
+#cjfee = ["%0.5f" % (0.00015 - n*0.00001) for n in range(mix_levels)]
+nickname = random_nick()
+nickserv_password = ''
+
+#END CONFIGURATION
+print cjfee
 
 #is a maker for the purposes of generating a yield from held
 # bitcoins without ruining privacy for the taker, the taker could easily check
@@ -32,9 +32,13 @@ mix_levels = 5
 # to ruin the privacy for barely any more yield
 #sell-side algorithm:
 #add up the value of each utxo for each mixing depth,
-# announce a relative-fee order of the highest balance
-#spent from utxos that try to make the highest balance even higher
-# so try to keep coins concentrated in one mixing depth
+# announce a relative-fee order of the balance in each mixing depth
+# amounts made to be non-overlapping
+# minsize set by the miner fee contribution, so you never earn less in cjfee than miner fee
+# cjfee drops as you go down to the lower-balance mixing depths, provides
+#  incentive for people to clump coins together for you in one mix depth
+#announce an absolute fee order between the dust limit and minimum amount
+# so that there is liquidity in the very low amounts too
 class YieldGenerator(Maker):
 	statement_file = os.path.join('logs', 'yigen-statement.csv')
 
@@ -65,35 +69,44 @@ class YieldGenerator(Maker):
 
 	def create_my_orders(self):
 		mix_balance = self.wallet.get_balance_by_mixdepth()
-		filtered_mix_balance = dict([(m, b) for m, b in mix_balance.iteritems() if b > common.DUST_THRESHOLD])
-		if len(filtered_mix_balance) == 0:
+		debug('mix_balance = ' + str(mix_balance))
+		nondust_mix_balance = dict([(m, b) for m, b in mix_balance.iteritems() if b > common.DUST_THRESHOLD])
+		if len(nondust_mix_balance) == 0:
 			debug('do not have any coins left')
 			return []
 		#sorts the mixdepth_balance map by balance size
-		filtered_mix_balance = sorted(list(mix_balance.iteritems()), key=lambda a: a[1], reverse=True)
-		minsize = int(1.5 * txfee / float(cjfee[0])) #minimum size is such that you always net profit at least 50% of the miner fee
-		filtered_mix_balance = [f for f in filtered_mix_balance if f[1] > minsize]
-		debug('minsize=' + str(minsize)) 
-		min_balances = filtered_mix_balance[1:] + [(-1, minsize)]
-		mix_balance_min = [(mxb[0], mxb[1], mnb[1]) for mxb, mnb in zip(filtered_mix_balance, min_balances)]
+		sorted_mix_balance = sorted(list(mix_balance.iteritems()), key=lambda a: a[1], reverse=True)
+		minsize = int(1.5 * txfee / float(min(cjfee))) #minimum size is such that you always net profit at least 50% of the miner fee
+		filtered_mix_balance = [f for f in sorted_mix_balance if f[1] > minsize]
+		debug('minsize=' + str(minsize) + ' calc\'d with cjfee=' + str(min(cjfee))) 
+		lower_bound_balances = filtered_mix_balance[1:] + [(-1, minsize)]
+		mix_balance_min = [(mxb[0], mxb[1], minb[1]) for mxb, minb in zip(filtered_mix_balance, lower_bound_balances)]
 		mix_balance_min = mix_balance_min[::-1] #reverse list order
+		thecjfee = cjfee[::-1]
 
 		debug('mixdepth_balance_min = ' + str(mix_balance_min))
 		orders=[]
-		for oid, mix_balance_min in enumerate(mix_balance_min):
-			mixdepth, balance, mins = mix_balance_min
+		oid = 0
+		for mix_bal_min in mix_balance_min:
+			mixdepth, balance, mins = mix_bal_min
 			#the maker class reads specific keys from the dict, but others
 			# are allowed in there and will be ignored
-			order = {'oid': oid+1, 'ordertype': 'relorder', 'minsize': mins - common.DUST_THRESHOLD + 1,
-				'maxsize': balance - common.DUST_THRESHOLD, 'txfee': txfee, 'cjfee': cjfee[oid],
+			order = {'oid': oid+1, 'ordertype': 'relorder', 'minsize': max(mins - common.DUST_THRESHOLD, common.DUST_THRESHOLD) + 1,
+				'maxsize': max(balance - common.DUST_THRESHOLD, common.DUST_THRESHOLD), 'txfee': txfee, 'cjfee': thecjfee[oid],
 				'mixdepth': mixdepth}
+			oid += 1
 			orders.append(order)
 
-		#the absorder is always oid=0
-		absorder_fee = calc_cj_fee('relorder', cjfee[oid], minsize)
-		order = {'oid': 0, 'ordertype': 'absorder', 'minsize': common.DUST_THRESHOLD + 1,
-			'maxsize': minsize - common.DUST_THRESHOLD, 'txfee': txfee, 'cjfee': absorder_fee}
-		orders = [order] + orders
+		absorder_size = min(minsize, sorted_mix_balance[0][1])
+		if absorder_size != 0:
+			lowest_cjfee = thecjfee[min(oid, len(thecjfee)-1)]
+			absorder_fee = calc_cj_fee('relorder', lowest_cjfee, minsize)
+			debug('absorder fee = ' + str(absorder_fee) + ' uses cjfee=' + str(lowest_cjfee))
+			#the absorder is always oid=0
+			order = {'oid': 0, 'ordertype': 'absorder', 'minsize': common.DUST_THRESHOLD + 1,
+				'maxsize': absorder_size - common.DUST_THRESHOLD, 'txfee': txfee, 'cjfee': absorder_fee}
+			orders = [order] + orders
+		debug('generated orders = \n' + '\n'.join([str(o) for o in orders]))
 		return orders
 
 	def oid_to_order(self, cjorder, oid, amount):
@@ -105,7 +118,7 @@ class YieldGenerator(Maker):
 			mix_balance = self.wallet.get_balance_by_mixdepth()
 			filtered_mix_balance = dict([(m, b) for m, b in mix_balance.iteritems() if b > common.DUST_THRESHOLD])
 			filtered_mix_balance = sorted(list(mix_balance.iteritems()), key=lambda a: a[1], reverse=True)
-			mixdepth = filtered_mix_balance[-1][0]
+			mixdepth = filtered_mix_balance[0][0]
 		debug('filling order, mixdepth=' + str(mixdepth))
 
 		#mixdepth is the chosen depth we'll be spending from
@@ -156,32 +169,41 @@ class YieldGenerator(Maker):
 
 		neworders = [o for o in myorders if o['ordertype'] == 'relorder']
 		oldorders = [o for o in oldorders if o['ordertype'] == 'relorder']
-		new_old_diff = [i for i, j in zip(sorted(neworders), sorted(oldorders)) if i != j]
-		old_new_diff = [i for i, j in zip(sorted(oldorders), sorted(neworders)) if i != j]
+		#new_setdiff_old = The relative complement of `new` in `old` = members in `new` which are not in `old`
+		new_setdiff_old = [o for o in neworders if o not in oldorders]
+		old_setdiff_new = [o for o in oldorders if o not in neworders]
 
 		debug('neworders = \n' + '\n'.join([str(o) for o in neworders]))
 		debug('oldorders = \n' + '\n'.join([str(o) for o in oldorders]))
-		debug('new_old_diff = \n' + '\n'.join([str(o) for o in new_old_diff]))
-		debug('old_new_diff = \n' + '\n'.join([str(o) for o in old_new_diff]))
+		debug('new_setdiff_old = \n' + '\n'.join([str(o) for o in new_setdiff_old]))
+		debug('old_setdiff_new = \n' + '\n'.join([str(o) for o in old_setdiff_new]))
 		if len(neworders) == len(oldorders):
-			ann_orders = new_old_diff
+			ann_orders = new_setdiff_old
 		elif len(neworders) > len(oldorders):
-			ann_orders = [o for o in neworders if o not in oldorders]
+			ann_orders = new_setdiff_old
 		elif len(neworders) < len(oldorders):
-			ann_orders = [o for o in neworders if o not in oldorders]
+			ann_orders = new_setdiff_old
 			ann_oids = [o['oid'] for o in ann_orders]
-			cancel_orders = [o['oid'] for o in old_new_diff if o['oid'] not in ann_oids]
+			cancel_orders = [o['oid'] for o in old_setdiff_new if o['oid'] not in ann_oids]
 
-		if len([o for o in self.orderlist if o['ordertype'] == 'absorder']) == 0:
-			absorder = [o for o in myorders if o['ordertype'] == 'absorder'][0]
-			ann_orders = [absorder] + ann_orders
+		#check if the absorder has changed, or if it needs to be newly announced
+		new_abs = [o for o in myorders if o['ordertype'] == 'absorder']
+		old_abs = [o for o in oldorders if o['ordertype'] == 'absorder']
+		if len(new_abs) > len(old_abs):
+			#announce an absorder where there wasnt one before
+			ann_orders = [new_abs[0]] + ann_orders
+		elif len(new_abs) == len(old_abs):
+			#maxsize is the only thing that changes, except cjfee but that changes at the same time
+			if new_abs[0]['maxsize'] != old_abs[0]['maxsize']:
+				ann_orders = [new_abs[0]] + ann_orders
 
-		debug('can_orders = ' + str(cancel_orders))
+		debug('can_orders = \n' + '\n'.join([str(o) for o in cancel_orders]))
 		debug('ann_orders = \n' + '\n'.join([str(o) for o in ann_orders]))
 		return (cancel_orders, ann_orders)
 
 	def on_tx_confirmed(self, cjorder, confirmations, txid):
 		confirm_time = int(time.time()) - self.tx_unconfirm_timestamp[cjorder.cj_addr]
+		del self.tx_unconfirm_timestamp[cjorder.cj_addr]
 		timestamp = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 		self.log_statement([timestamp, cjorder.cj_amount, len(cjorder.utxos),
 			sum([av['value'] for av in cjorder.utxos.values()]), cjorder.real_cjfee,
