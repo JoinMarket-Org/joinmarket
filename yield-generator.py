e0ab8ae69b047eb4a50c5f56225eeeb080aa40de
chris-belcher <chris-belcher@users.noreply.github.com>
2015-07-11 01:59:47 +0100

    fixed bug involving wrong value for minsize, added more debug lines

--- yield-generator.py
+++ yield-generator.py
@@ -69,15 +69,16 @@ class YieldGenerator(Maker):
 
 	def create_my_orders(self):
 		mix_balance = self.wallet.get_balance_by_mixdepth()
+		debug('mix_balance = ' + str(mix_balance))
 		nondust_mix_balance = dict([(m, b) for m, b in mix_balance.iteritems() if b > common.DUST_THRESHOLD])
 		if len(nondust_mix_balance) == 0:
 			debug('do not have any coins left')
 			return []
 		#sorts the mixdepth_balance map by balance size
 		sorted_mix_balance = sorted(list(mix_balance.iteritems()), key=lambda a: a[1], reverse=True)
-		minsize = int(1.5 * txfee / float(cjfee[0])) #minimum size is such that you always net profit at least 50% of the miner fee
+		minsize = int(1.5 * txfee / float(min(cjfee))) #minimum size is such that you always net profit at least 50% of the miner fee
 		filtered_mix_balance = [f for f in sorted_mix_balance if f[1] > minsize]
-		debug('minsize=' + str(minsize)) 
+		debug('minsize=' + str(minsize) + ' calc\'d with cjfee=' + str(min(cjfee))) 
 		min_balances = filtered_mix_balance[1:] + [(-1, minsize)]
 		mix_balance_min = [(mxb[0], mxb[1], mnb[1]) for mxb, mnb in zip(filtered_mix_balance, min_balances)]
 		mix_balance_min = mix_balance_min[::-1] #reverse list order
@@ -103,6 +104,7 @@ class YieldGenerator(Maker):
 		order = {'oid': 0, 'ordertype': 'absorder', 'minsize': common.DUST_THRESHOLD + 1,
 			'maxsize': absorder_size - common.DUST_THRESHOLD, 'txfee': txfee, 'cjfee': absorder_fee}
 		orders = [order] + orders
+		debug('generated orders = \n' + pprint.pformat(orders))
 		return orders
 
 	def oid_to_order(self, cjorder, oid, amount):
