d910fed50347c6f077e9bf7bccad3b4f3ddb9ad3
Adlai Chandrasekhar <adlai.chandrasekhar@gmail.com>
2015-08-06 02:58:34 +0300

    add ?scale=log to /ordersize

--- ob-watcher.py
+++ ob-watcher.py
@@ -55,21 +55,30 @@ def create_depth_chart(db, cj_amount):
 	plt.ylabel('Frequency')
 	return get_graph_html(fig)
 
-def create_size_histogram(db):
+def create_size_histogram(db, args):
 	try:
 		import matplotlib.pyplot as plt
 	except ImportError:
 		return 'Install matplotlib to see graphs'
 	rows = db.execute('SELECT maxsize FROM orderbook;').fetchall()
-	ordersizes = [r['maxsize']/1e8 for r in rows]
+	ordersizes = sorted([r['maxsize']/1e8 for r in rows])
 
 	fig = plt.figure()
-	plt.hist(ordersizes, 30, histtype='bar', rwidth=0.8)
+        scale = args.get("scale")
+	if (scale is not None) and (scale[0] == "log"):
+		ratio = ordersizes[-1] / ordersizes[0]
+		step = ratio ** 0.0333 # 1/30
+		bins = [ordersizes[0] * (step ** i) for i in range(30)]
+	else:
+		bins = 30
+	plt.hist(ordersizes, bins, histtype='bar', rwidth=0.8)
+	if bins is not 30:
+		fig.axes[0].set_xscale('log')
 	plt.grid()
-	#plt.title('Order size distribution')
 	plt.xlabel('Order sizes / btc')
 	plt.ylabel('Frequency')
-	return get_graph_html(fig)
+	return get_graph_html(fig) + ("<br/><a href='?scale=log'>log scale</a>"
+				      if bins == 30 else "<br/><a href='?'>linear</a>")
 
 def get_graph_html(fig):
 	imbuf = io.BytesIO()
@@ -161,7 +170,7 @@ class OrderbookPageRequestHeader(SimpleHTTPServer.SimpleHTTPRequestHandler):
 				'PAGETITLE': 'JoinMarket Browser Interface',
 				'MAINHEADING': 'Order Sizes',
 				'SECONDHEADING': 'Order Size Histogram' + alert_msg,
-				'MAINBODY': create_size_histogram(self.taker.db)
+				'MAINBODY': create_size_histogram(self.taker.db, args)
 			}
 		elif self.path.startswith('/depth'):
 			#if self.path[6] == '?':
