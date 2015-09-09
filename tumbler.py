d993efcf9f312a8c4d5d3a91ada5313697e042b9
chris-belcher <chris-belcher@users.noreply.github.com>
2015-09-09 23:34:50 +0100

    fixed bug from merge with #172, rearranged stuff so walletnotify could work, fixed some spelling mistakes

--- tumbler.py
+++ tumbler.py
@@ -132,7 +132,7 @@ class TumblerThread(threading.Thread):
 					debug('waiting for liquidity 1min, hopefully more orders should come in')
 					time.sleep(60)
 					continue
-				cj_fee = 1.0*(total_value - cjamount) / tx['makercount'] / cjamount
+				cj_fee = 1.0*(total_value - cj_amount) / self.tx['makercount'] / cj_amount
 				debug('average fee = ' + str(cj_fee))
 				if cj_fee > self.taker.maxcjfee:
 					print 'cj fee higher than maxcjfee at ' + str(cj_fee) + ', waiting 60 seconds'
