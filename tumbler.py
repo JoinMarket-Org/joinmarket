b77921c92e5c0dd7c339f45f03fa61e9e547db72
chris-belcher <chris-belcher@users.noreply.github.com>
2015-09-19 01:00:20 +0100

    fixed bug where tumbler didnt take into account fees
    [gitreformat yapf-ify (github/ghtdak) on Fri Dec  4 04:51:40 2015]
    [from commit: cd237954fa7cd759a788b56d4ca2c83290d7f9e8]

--- tumbler.py
+++ tumbler.py
@@ -179,7 +179,7 @@ class TumblerThread(threading.Thread):
             total_amount = cj_amount + total_cj_fee + self.taker.txfee
             debug('total amount spent = ' + str(total_amount))
             utxos = self.taker.wallet.select_utxos(self.tx['srcmixdepth'],
-                                                   cj_amount)
+                                                   total_amount)
             choose_orders_recover = self.tumbler_choose_orders
 
         self.taker.start_cj(self.taker.wallet, cj_amount, orders, utxos,
