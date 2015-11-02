647451970f821d94d2488e334f3fb03a628380f2
arubi <arubi@freenode>
2015-11-03 00:15:55 +0200

    Rewrote install.sh
    Added submodule init and update to install.sh
    
    Fixed type at install.sh:93
    
    - Switched to fetching from python.org
    - Added handling for symlinks
    
    - Added ptyprocess as submodule (needed for tests)
    - Added ptyprocess build to install.sh
    
    - Added checksum checks for cffi and gmp
    - Now using gmp 6.1.0
    - Replaced bash with sh
    
    Added installation instructions for Ubuntu and Debian
    
    Fix typos with paths
    
    Fix more typos
    
    Now using two spaces ("  ") in sha256sum and md5sum to support
    earlier versions
    
    [gitreformat yapf-ify (github/ghtdak) on Fri Dec  4 04:56:34 2015]
    [from commit: 98e14f7479d13b53e4485df892ca299362176896]

--- install.sh
+++ install.sh
@@ -1,55 +1,119 @@
-#!/bin/bash
+#!/bin/sh
 set -e
-clear
 
-#check for root
-if [ "$(id -u)" = "0" ]; then
-echo "You should not run as root;
-you will be prompted for the admin password when needed."
-read -p "Press ENTER to exit the script, and run again, not as root."
-exit 0
-fi
+joinmarket_home="$(dirname $(readlink -f "$0"))"
 
-echo "This script will install JOINMARKET on your computer.
-You are strongly recommended to run it with a local Bitcoin Core
-instance for extra privacy. See the Joinmarket wiki for details."
-read -p "Press ENTER to continue."
+git submodule init
+git submodule update
 
-echo "Will now update apt"
-#sudo apt-get update
-clear
+# Downloading and unpacking cffi
+rm -f ${joinmarket_home}/deps/cffi-1.3.0.tar.gz
+wget -t 5 https://pypi.python.org/packages/source/c/cffi/cffi-1.3.0.tar.gz -O ${joinmarket_home}/deps/cffi-1.3.0.tar.gz
+md5sum -c <<E
+a40ed8c8ac653c8fc7d5603711b06eaf  ./deps/cffi-1.3.0.tar.gz
+E
+tar -C ./deps -xzf ${joinmarket_home}/deps/cffi-1.3.0.tar.gz
 
-echo "Will now install dependencies"
-sudo apt-get install -y build-essential
-sudo apt-get install -y automake
-#required for secp256k1
-sudo apt-get install -y libgmp-dev
-pip install pexpect
-pip install cffi
+# Downloading and unpacking libgmp
+rm -f ${joinmarket_home}/deps/gmp-6.1.0.tar.bz2
+wget -t 5 https://gmplib.org/download/gmp/gmp-6.1.0.tar.bz2 -O ${joinmarket_home}/deps/gmp-6.1.0.tar.bz2
+bunzip2 -f ${joinmarket_home}/deps/gmp-6.1.0.tar.bz2
+sha256sum -c <<E
+7afbeccd5b248a4996ad9ff08f66dc46d30fa53364ba436dfb86d67fc4a35848  ./deps/gmp-6.1.0.tar
+E
+tar -C ./deps -xf ${joinmarket_home}/deps/gmp-6.1.0.tar
 
-echo "will now get libsodium and secp256k1 and compile."
-git clone git://github.com/jedisct1/libsodium.git
-cd libsodium
-git checkout tags/1.0.3
+# Building libffi
+
+libffi_root=${joinmarket_home}/deps/build/libffi
+cd ${joinmarket_home}/deps/libffi/
+./configure --prefix=${libffi_root}
+make
+make install
+
+
+# Building ptyprocess
+
+ptyprocess_root=${joinmarket_home}/deps/build/ptyprocess
+cd ${joinmarket_home}/deps/ptyprocess
+python setup.py build
+python setup.py install --prefix=${ptyprocess_root}
+
+
+# Building pexpect
+
+pexpect_root=${joinmarket_home}/deps/build/pexpect
+cd ${joinmarket_home}/deps/pexpect
+export PYTHONPATH=${ptyprocess_root}/lib/python2.7/site-packages/:${PYTHONPATH}
+python setup.py build
+python setup.py install --prefix=${pexpect_root}
+
+
+# Building pycparser
+
+pycparser_root=${joinmarket_home}/deps/build/pycparser
+cd ${joinmarket_home}/deps/pycparser
+python setup.py build
+python setup.py install --prefix=${pycparser_root}
+
+
+# Building cffi
+
+cffi_root=${joinmarket_home}/deps/build/cffi
+cd ${joinmarket_home}/deps/cffi-1.3.0
+
+export C_INCLUDE_PATH=${libffi_root}/lib/libffi-3.0.13/include:${C_INCLUDE_PATH}
+export LIBRARY_PATH=${libffi_root}/lib:${LIBRARY_PATH}
+
+python setup_base.py build
+python setup_base.py install --prefix=${cffi_root}
+ln -sf ${joinmarket_home}/deps/cffi-1.3.0/cffi/_cffi_include.h ${cffi_root}/lib/python2.7/site-packages/cffi/
+ln -sf ${joinmarket_home}/deps/cffi-1.3.0/cffi/parse_c_type.h ${cffi_root}/lib/python2.7/site-packages/cffi/
+
+# Building libsodium
+
+libsodium_root=${joinmarket_home}/deps/build/libsodium
+cd ${joinmarket_home}/deps/libsodium
 ./autogen.sh
-./configure
+./configure --prefix=${libsodium_root}
+make
+make install
+
+
+# Building gmp
+
+gmp_root=${joinmarket_home}/deps/build/gmp
+cd ${joinmarket_home}/deps/gmp-6.1.0
+./configure --prefix=${gmp_root}
+make
 make check
-sudo make install
-cd ..
+make install
 
-git clone git://github.com/bitcoin/secp256k1.git
-cd secp256k1
+
+# Building secp256k1
+
+secp256k1_root=${joinmarket_home}/deps/build/secp256k1
+cd ${joinmarket_home}/deps/secp256k1
+
 ./autogen.sh
-./configure --enable-module-ecdh --enable-module-schnorr --enable-module-recovery
+export C_INCLUDE_PATH=${gmp_root}/include/:${C_INCLUDE_PATH}
+export LIBRARY_PATH=${gmp_root}/lib:${LIBRARY_PATH}
+./configure --enable-module-ecdh --enable-module-schnorr --enable-module-recovery --prefix=${secp256k1_root}
 make
 ./tests
-sudo make install
-cd ..
-sudo ldconfig
-#set up python bindings
-cd lib/bitcoin
+make install
+
+
+# Building joinmarket 
+
+cd ${joinmarket_home}/lib/bitcoin
+
+export C_INCLUDE_PATH=${libsodium_root}/include:${secp256k1_root}/include:${C_INCLUDE_PATH}
+export LIBRARY_PATH=${libsodiom_root}/lib:${secp256k1_root}/lib:${LIBRARY_PATH}
+export PYTHONPATH=${pycparser_root}/lib/python2.7/site-packages:${cffi_root}/lib/python2.7/site-packages/:${pexpect_root}/lib/python2.7/site-packages/:${ptyprocess_root}/lib/python2.7/site-packages/:${PYTHONPATH}
 python build.py
 python noncefunc_build.py
-cd ../..
-#tests
-python lib/enc_wrapper.py
+
+cd ${joinmarket_home}/lib
+export LD_LIBRARY_PATH=${libsodium_root}/lib:${secp256k1_root}/lib:${LD_LIBRARY_PATH}
+python enc_wrapper.py
