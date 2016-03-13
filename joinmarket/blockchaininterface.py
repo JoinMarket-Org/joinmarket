b1b93e40412a8bbc1a1723a9f7aec986bd042c56
Michael Wozniak <github@koh.ms>
2016-03-13 15:51:27 -0400

    update transaction get response parsing

--- joinmarket/blockchaininterface.py
+++ joinmarket/blockchaininterface.py
@@ -642,7 +642,7 @@ class ElectrumInterface(BlockchainInterface):
                         continue
                     data = []
                     for txid in shared_txid:
-                        txdata = self.blockchaininterface.get_from_electrum('blockchain.transaction.get', txid).get('result')
+                        txdata = str(self.blockchaininterface.get_from_electrum('blockchain.transaction.get', txid).get('result'))
                         data.append({'hex':txdata,'id':txid})
                     for txdata in data:
                         txhex = txdata['hex']
@@ -674,7 +674,7 @@ class ElectrumInterface(BlockchainInterface):
                         continue
                     data = []
                     for txid in shared_txid:
-                        txdata = self.blockchaininterface.get_from_electrum('blockchain.transaction.get', txid).get('result')
+                        txdata = str(self.blockchaininterface.get_from_electrum('blockchain.transaction.get', txid).get('result'))
                         data.append({'hex':txdata,'id':txid})
                     for txdata in data:
                         txhex = txdata['hex']
