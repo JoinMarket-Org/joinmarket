b0d3d6772bbc04b88200d1e65ddfadf5a3f52654
Adam Gibson <ekaggata@gmail.com>
2016-03-24 22:22:10 +0200

    test private key formats coverage now includes yieldgen/maker code due to graceful subprocess shutdown add sweep transaction to regtest tests add tests for utxo selection algorithms and pragma no cover for abstract methods

---
+++ test/test_keys.py
@@ -0,0 +1,66 @@
+#! /usr/bin/env python
+from __future__ import absolute_import
+'''Public and private key validity and formatting tests.'''
+
+import bitcoin as btc
+import binascii
+from joinmarket.configure import load_program_config, get_p2pk_vbyte
+from joinmarket import jm_single
+import json
+import pytest
+
+
+def test_wif_privkeys_invalid(setup_keys):
+    #none of these are valid as any kind of key or address
+    with open("test/base58_keys_invalid.json", "r") as f:
+        json_data = f.read()
+    invalid_key_list = json.loads(json_data)
+    for k in invalid_key_list:
+        bad_key = k[0]
+        for netval in ["mainnet", "testnet"]:
+            jm_single().config.set("BLOCKCHAIN", "network", netval)
+            #if using py.test -s ; sanity check to see what's actually being tested
+            print 'testing this key: ' + bad_key
+            try:
+                bad_key_format = btc.get_privkey_format(bad_key)
+                print 'has correct format: ' + bad_key_format
+            except:
+                pass
+            #should throw exception
+            with pytest.raises(Exception) as e_info:
+                from_wif_key = btc.decode_privkey(bad_key)
+                #in case the b58 check encoding is valid, we should
+                #also check if the leading version byte is in the
+                #expected set, and throw an error if not.
+                if chr(btc.get_version_byte(bad_key)) not in '\x80\xef':
+                    raise Exception("Invalid version byte")
+                #open to discussion: the bitcoin library should throw
+                #if the compression byte is not there (discussion open 
+                #because of secp256k1, which will not accept invalid compressed
+                #raw keys)
+                if "compressed" in btc.get_privkey_format(bad_key) and \
+                   btc.b58check_to_bin(x)[-1] != '\x01':
+                    raise Exception("Invalid compression byte")
+
+
+def test_wif_privkeys_valid(setup_keys):
+    with open("test/base58_keys_valid.json", "r") as f:
+        json_data = f.read()
+    valid_keys_list = json.loads(json_data)
+    for a in valid_keys_list:
+        key, hex_key, prop_dict = a
+        if prop_dict["isPrivkey"]:
+            netval = "testnet" if prop_dict["isTestnet"] else "mainnet"
+            jm_single().config.set("BLOCKCHAIN", "network", netval)
+            print 'testing this key: ' + key
+            assert chr(btc.get_version_byte(
+                key)) in '\x80\xef', "not valid network byte"
+            from_wif_key = btc.decode_privkey(key)
+            expected_key = btc.decode_privkey(hex_key)
+            assert from_wif_key == expected_key, "Incorrect key decoding: " + \
+                   str(from_wif_key) + ", should be: " + str(expected_key)
+
+
+@pytest.fixture(scope="module")
+def setup_keys():
+    load_program_config()
--- test/test_wallets.py
+++ test/test_wallets.py
@@ -10,15 +10,53 @@ import pexpect
 import random
 import subprocess
 import unittest
-from commontest import local_command, interact
+from commontest import local_command, interact, make_wallets
 
 import bitcoin as btc
-
+import pytest
 from joinmarket import load_program_config, jm_single
 from joinmarket import get_p2pk_vbyte, get_log, Wallet
-from joinmarket.support import chunks
+from joinmarket.support import chunks, select_gradual, \
+     select_greedy, select_greediest
 
 log = get_log()
+
+
+@pytest.mark.parametrize(
+    "nw, wallet_structures, mean_amt, sdev_amt, amount",
+    [
+        #TODO create structures that cover edge cases like
+        #"return [high[0]]" in select_greediest
+        (3, [[1, 0, 0, 0, 0], [0, 4, 2, 0, 1], [2, 6, 0, 0, 0]], 4, 1.4,
+         800000000),
+    ])
+def test_utxo_selection(setup_wallets, nw, wallet_structures, mean_amt,
+                        sdev_amt, amount):
+    """Check that all the utxo selection algorithms work with a random
+    variety of wallet contents.
+    """
+    wallets = make_wallets(nw, wallet_structures, mean_amt, sdev_amt)
+    for w in wallets.values():
+        jm_single().bc_interface.sync_wallet(w['wallet'])
+    for k, w in enumerate(wallets.values()):
+        for algo in [select_gradual, select_greedy, select_greediest, None]:
+            wallet = w['wallet']
+            if algo:
+                wallet.utxo_selector = algo
+            if k == 0:
+                with pytest.raises(Exception) as e_info:
+                    selected = wallet.select_utxos(1, amount)
+            else:
+                selected = wallet.select_utxos(1, amount)
+                algostr = algo.__name__ if algo else "default"
+                print 'selected these for algo ' + algostr + ':'
+                print selected
+                #basic check:
+                #does this algo actually generate sufficient coins?
+                total_selected = sum([x['value'] for x in selected.values()])
+                assert total_selected > amount, "Selection algo: " + algo + \
+                       "failed to select sufficient coins, total: " + \
+                       str(total_selected) + ", should be: " + str(amount)
 
 
 class TestWalletCreation(unittest.TestCase):
@@ -106,3 +144,8 @@ class TestWalletRecovery(unittest.TestCase):
         except:
             return False
         return True
+
+
+@pytest.fixture(scope="module")
+def setup_wallets():
+    load_program_config()
