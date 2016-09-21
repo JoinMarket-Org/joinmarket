c3047c793bed2776b058ea29913c9e5eb284de3d
Bryan Stitt <bryan@stitthappens.com>
2016-09-20 18:46:25 -0700

    autopep8 -a -a -a -a -i yg-pe.py

--- yg-pe.py
+++ yg-pe.py
@@ -26,24 +26,25 @@ log = get_log()
 # bitcoins while maximising the difficulty of spying on activity;
 # this is primarily attempted by avoiding reannouncemnt of orders
 # after transactions whereever that is possible.
-class YieldGeneratorPrivEnhance(YieldGenerator):
 
+
+class YieldGeneratorPrivEnhance(YieldGenerator):
 
     def __init__(self, msgchan, wallet, offerconfig):
         self.txfee, self.cjfee_a, self.cjfee_r, self.ordertype, self.minsize, \
             self.mix_levels = offerconfig
-        super(YieldGeneratorPrivEnhance,self).__init__(msgchan, wallet)
+        super(YieldGeneratorPrivEnhance, self).__init__(msgchan, wallet)
 
     def create_my_orders(self):
         mix_balance = self.wallet.get_balance_by_mixdepth()
-        #We publish ONLY the maximum amount and use minsize for lower bound;
-        #leave it to oid_to_order to figure out the right depth to use.
+        # We publish ONLY the maximum amount and use minsize for lower bound;
+        # leave it to oid_to_order to figure out the right depth to use.
         f = '0'
         if ordertype == 'reloffer':
             f = self.cjfee_r
-            #minimum size bumped if necessary such that you always profit 
-            #least 50% of the miner fee
-            self.minsize = int(1.5 * self.txfee / float(self.cjfee_r))            
+            # minimum size bumped if necessary such that you always profit
+            # least 50% of the miner fee
+            self.minsize = int(1.5 * self.txfee / float(self.cjfee_r))
         elif ordertype == 'absoffer':
             f = str(self.txfee + self.cjfee_a)
         mix_balance = dict([(m, b) for m, b in mix_balance.iteritems()
@@ -86,21 +87,22 @@ class YieldGeneratorPrivEnhance(YieldGenerator):
 
         log.debug('mix depths that have enough = ' + str(filtered_mix_balance))
 
-        #Avoid the max mixdepth wherever possible, to avoid changing the
-        #offer. Algo: 
+        # Avoid the max mixdepth wherever possible, to avoid changing the
+        # offer. Algo:
         #"mixdepth" is the mixdepth we are spending FROM, so it is also
-        #the destination of change.
+        # the destination of change.
         #"cjoutdepth" is the mixdepth we are sending coinjoin out to.
         #
-        #Find a mixdepth, in the set that have enough, which is
-        #not the maximum, and choose any from that set as "mixdepth".
-        #If not possible, it means only the max_mix depth has enough,
-        #so must choose "mixdepth" to be that.
-        #To find the cjoutdepth: ensure that max != min, if so it means
-        #we had only one depth; in that case, just set "cjoutdepth"
-        #to the next mixdepth. Otherwise, we set "cjoutdepth" to the minimum.
+        # Find a mixdepth, in the set that have enough, which is
+        # not the maximum, and choose any from that set as "mixdepth".
+        # If not possible, it means only the max_mix depth has enough,
+        # so must choose "mixdepth" to be that.
+        # To find the cjoutdepth: ensure that max != min, if so it means
+        # we had only one depth; in that case, just set "cjoutdepth"
+        # to the next mixdepth. Otherwise, we set "cjoutdepth" to the minimum.
 
-        nonmax_mix_balance = [m for m in filtered_mix_balance if m[0] != max_mix]
+        nonmax_mix_balance = [
+            m for m in filtered_mix_balance if m[0] != max_mix]
         if not nonmax_mix_balance:
             log.debug("Could not spend from a mixdepth which is not max")
             mixdepth = max_mix
@@ -110,10 +112,11 @@ class YieldGeneratorPrivEnhance(YieldGenerator):
 
         # mixdepth is the chosen depth we'll be spending from
         # min_mixdepth is the one we want to send our cjout TO,
-        # to minimize chance of it becoming the largest, and reannouncing offer.
+        # to minimize chance of it becoming the largest, and reannouncing
+        # offer.
         if mixdepth == min_mix:
             cjoutmix = (mixdepth + 1) % self.wallet.max_mix_depth
-            #don't send cjout to max
+            # don't send cjout to max
             if cjoutmix == max_mix:
                 cjoutmix = (cjoutmix + 1) % self.wallet.max_mix_depth
         else:
