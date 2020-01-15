e0b1e0804b49558475bab32db3f30a4f59435040
chris-belcher <chris-belcher@users.noreply.github.com>
2020-01-15 13:04:16 +0000

    Wipe files and modify readme to link to -clientserver
    Anybody who needs the old files can access them using git reset

--- bitcoin/__init__.py
+++
@@ -1,8 +0,0 @@
-from bitcoin.py2specials import *
-from bitcoin.py3specials import *
-import secp256k1
-from bitcoin.secp256k1_main import *
-from bitcoin.secp256k1_transaction import *
-from bitcoin.secp256k1_deterministic import *
-from bitcoin.bci import *
-from bitcoin.podle import *
--- test/wnb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in {62612..62619}
-do
-curl -sI --max-time 0.6 http://localhost:$i/walletnotify?$1
-done
-
